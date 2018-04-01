664d895acd2f2c39a038668481379d3a95be60aa
Lino Virgen <lino.virgen96@gmail.com>
2018-04-01 02:12:54 -0500

    well updated

--- Assets/Scripts/GenerateWells.cs
+++ Assets/Scripts/GenerateWells.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class GenerateWells : MonoBehaviour {
 
     // Use this for initialization
+
+	public GameObject water_well_prefab;
    
     void Start () {
         TextAsset txtAsset = (TextAsset)Resources.Load("data", typeof(TextAsset));
@@ -13,20 +15,18 @@ public class GenerateWells : MonoBehaviour {
         for(int index =1;index < lines.Length-1; index++)
         {
             string[] values = lines[index].Split(',');
-            Debug.Log(values);
+            // Debug.Log(values);
             float longitude = float.Parse(values[2]);
             float latitude = float.Parse(values[3]);
 			if (longitude >= -102.0156f && longitude <= -101.74713)
 			{
 				if (latitude >= 33.47297 && latitude <= 33.69849) 
 				{
-					GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-					cube.name = values[0];
 					float xPos = (longitude - -102.0156f) * 1862.28756f;
 					float zPos = (latitude - 33.47297f) * 2217.098262f;
-					cube.transform.position = new Vector3 (xPos, 110f, zPos);
-					cube.AddComponent<DisplayInfo> ();
-					cube.GetComponent<DisplayInfo> ().inFormation = "X: " + values [1] + " Y: " + values [2] + " Z: " + values [3];
+					var well = Instantiate(water_well_prefab, new Vector3 (xPos, 120f, zPos), Quaternion.identity);
+					well.name = values [0];
+					well.GetComponent<DisplayInfo> ().inFormation = "X: " + values [1] + " Y: " + values [2] + " Z: " + values [3];
 				}
 			}
         }
