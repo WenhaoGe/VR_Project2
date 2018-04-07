e41efe6f5be0922230eec5a117c1ddd45a6c0091
Lino Virgen <lino.virgen96@gmail.com>
2018-04-07 18:10:49 -0500

    added underground water layer

--- Assets/Scripts/GenerateWells.cs
+++ Assets/Scripts/GenerateWells.cs
@@ -31,9 +31,9 @@ public class GenerateWells : MonoBehaviour {
 
 					var well = Instantiate(water_well_prefab, new Vector3 (xPos, 300f, zPos), Quaternion.identity);
 					var marker = Instantiate(well_marker, new Vector3 (xPos, 350f, zPos), Quaternion.Euler(new Vector3(80,0,0)));
-                    var depth = Instantiate(depth_object, new Vector3(xPos, 100f, zPos), Quaternion.identity);
-					var acc = new Vector3(0.0f, -2.5f, 0.0f);
-					depth.GetComponent<Rigidbody>().AddForce(-acc*250);
+                    var depth = Instantiate(depth_object, new Vector3(xPos, 170f, zPos), Quaternion.identity);
+					var acc = new Vector3(0.0f, -5.0f, 0.0f);
+					depth.GetComponent<Rigidbody>().AddForce(-acc*500);
 
                     depth.transform.localScale = new Vector3(1.0f, 0.02f * elevation, 1.0f);
 
