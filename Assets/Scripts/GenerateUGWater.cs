bd6d79764146ad9039018119734351938883f2bf
Lino Virgen <lino.virgen96@gmail.com>
2018-04-07 00:48:29 -0500

    saturated thickness layer added

---
+++ Assets/Scripts/GenerateUGWater.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateUGWater : MonoBehaviour {
+
+    // Use this for initialization
+
+	public GameObject water_cube;
+   
+    void Start ()
+	{
+        TextAsset txtAsset = (TextAsset)Resources.Load("raster_to_point", typeof(TextAsset));
+        string[] lines = txtAsset.text.Split('\n');
+        Debug.Log(lines.Length);
+        for(int index =1;index < lines.Length-1; index++)
+        {
+            string[] values = lines[index].Split(',');
+            float longitude = float.Parse(values[2]);
+            float latitude = float.Parse(values[3]);
+            float thickness = float.Parse(values[1]);
+
+			if (longitude >= -102.0156f && longitude <= -101.74713)
+			{
+				if (latitude >= 33.47297 && latitude <= 33.69849) 
+				{
+					float xPos = (longitude - -102.0156f) * 1862.28756f;
+					float zPos = (latitude - 33.47297f) * 2217.098262f;
+                    var point = Instantiate(water_cube, new Vector3(xPos, 100f, zPos), Quaternion.identity);
+					point.transform.localScale = new Vector3(45.0f, 0.02f * thickness, 45.0f);
+
+
+					point.name = values [0];
+				}
+			}
+        }
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/GenerateWells.cs
+++ Assets/Scripts/GenerateWells.cs
@@ -10,7 +10,8 @@ public class GenerateWells : MonoBehaviour {
 	public GameObject well_marker;
     public GameObject depth_object;
    
-    void Start () {
+    void Start ()
+	{
         TextAsset txtAsset = (TextAsset)Resources.Load("data", typeof(TextAsset));
         string[] lines = txtAsset.text.Split('\n');
         Debug.Log(lines.Length);
@@ -30,13 +31,15 @@ public class GenerateWells : MonoBehaviour {
 
 					var well = Instantiate(water_well_prefab, new Vector3 (xPos, 300f, zPos), Quaternion.identity);
 					var marker = Instantiate(well_marker, new Vector3 (xPos, 350f, zPos), Quaternion.Euler(new Vector3(80,0,0)));
-                    var depth = Instantiate(depth_object, new Vector3(xPos, 130f, zPos), Quaternion.identity);
-					var acc = new Vector3(0.0f, -10.0f, 0.0f);
-					depth.GetComponent<Rigidbody>().AddForce(-acc*1000);
+                    var depth = Instantiate(depth_object, new Vector3(xPos, 100f, zPos), Quaternion.identity);
+					var acc = new Vector3(0.0f, -2.5f, 0.0f);
+					depth.GetComponent<Rigidbody>().AddForce(-acc*250);
 
-                    //depth.transform.localScale = new Vector3(1.0f, 0.02f * elevation, 1.0f);
+                    depth.transform.localScale = new Vector3(1.0f, 0.02f * elevation, 1.0f);
 
 					well.name = values [0];
+					marker.name = values[0]+"_marker";
+					depth.name = values[0]+"_elevation";
 					var info = "\nLocation: "+ longitude +", "+latitude+"\nCounty: "+values[4];
 					var info2 = "\nMeasurement: " + values [5] + "/" + values [6] + "/" + values [7];
 					var info3 = "\nWater Elevation: " + values [8] + "\nSaturatedThickness: " + values [9];
