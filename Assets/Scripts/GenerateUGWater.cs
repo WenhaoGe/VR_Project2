8fa528f5ebceeb99c5149629bc2c5c000d6acf0d
WenhaoGe <wenhao.ge@ttu.edu>
2018-04-08 21:14:22 -0500

    Added comments,changed scripts

--- Assets/Scripts/GenerateUGWater.cs
+++ Assets/Scripts/GenerateUGWater.cs
@@ -7,7 +7,42 @@ public class GenerateUGWater : MonoBehaviour {
     // Use this for initialization
 
 	public GameObject water_cube;
-   
+
+    public void ReadFile()
+    {
+        TextAsset txtAsset = (TextAsset)Resources.Load("precipitation_data", typeof(TextAsset));
+        string[] lines = txtAsset.text.Split(new char[] { '\n' });
+
+
+
+        string[] values = lines[1].Split(',');
+        //print(values[1]);
+        float numVal1, numVal2, numVal3, numVal4, numVal5, numVal6, numVal7, numVal8, numVal9, numVal10, numVal11, numVal12;
+        float.TryParse(values[1], out numVal1);
+        float.TryParse(values[2], out numVal2);
+        float.TryParse(values[3], out numVal3);
+        float.TryParse(values[4], out numVal4);
+        float.TryParse(values[5], out numVal5);
+        float.TryParse(values[6], out numVal6);
+        float.TryParse(values[7], out numVal7);
+        float.TryParse(values[8], out numVal8);
+        float.TryParse(values[9], out numVal9);
+        float.TryParse(values[10], out numVal10);
+        float.TryParse(values[11], out numVal11);
+        float.TryParse(values[12], out numVal12);
+
+        // calculate the average precipitation data of four seasons
+        double average_Spring = (numVal1 + numVal11 + numVal12) / 3;
+        double average_Summer = (numVal2 + numVal3 + numVal4) / 3;
+        double average_Fall = (numVal5 + numVal6 + numVal7) / 3;
+        double average_Winter = (numVal8 + numVal9 + numVal10) / 3;
+
+        /*  print(average_Spring);
+          print(average_Summer);
+          print(average_Fall);
+          print(average_Winter);*/
+    }
+
     void Start ()
 	{
         TextAsset txtAsset = (TextAsset)Resources.Load("raster_to_point", typeof(TextAsset));
